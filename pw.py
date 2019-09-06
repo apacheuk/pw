fbc7bab2b86ba14adaa9c7bbd95105ec7af9195b
Simon Redmond <redmond.simonuk@gmail.com>
2019-09-06 15:40:34 +0100

    initial commit

---
+++ pw.py
@@ -0,0 +1,57 @@
+"""PW CLI
+
+Has a dependency on the app xsel being installed
+install with:
+	sudo apt install xsel
+
+Usage:
+pw.py [--size=<sz>] [--copy]
+pw.py -h|--help
+pw.py -v|--version
+
+Options:
+--copy automatically copy newly created password to the clipboard
+-h --help  Show this screen.
+-v --version  Show version.
+"""
+from docopt import docopt
+from random import choice
+from subprocess import Popen, PIPE
+
+
+def mkpassword(psize):
+    pwd = []
+    charsets = [
+        'abcdefghijklmnopqrstuvwxyz',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        '0123456789',
+        '^!\$%&/()=?{[]}+~#-_.:,;<>|\\',
+    ]
+    charset = choice(charsets)
+    while len(pwd) < psize:
+        pwd.append(choice(charset))
+        charset = choice(list(set(charsets) - set([charset])))
+    return "".join(pwd)
+
+
+def paste(str):
+
+    p = Popen(['xsel', '-bi'], stdin=PIPE)
+    p.communicate(input=str)
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='pw 0.0.1a')
+
+    size = int(arguments.get('--size'))
+    clip = arguments.get('--copy')
+
+    if size is None:
+        # default the password size if no length passed in
+        size = 12
+
+    x = mkpassword(psize=size)
+    print(x)
+
+    if clip is True:
+    	paste(x)
